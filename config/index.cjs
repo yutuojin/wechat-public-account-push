b96216b26ca9e6e2948a0e4bdf5a457ec8b9438d
yutuojin <116081538+yutuojin@users.noreply.github.com>
2022-11-24 11:20:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx65ed5f4e79597cf3',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'fa9ddd01ce16c71c0dff639b7f63e513',
 
   PROVINCE: '昆明',
   CITY: '嵩明',
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'Yu',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wx65ed5f4e79597cf3',
+      id: 'oZ-s-5l-ZC_TC-HmbtBfNEPowWno',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'NeWJcTqTtOW0Y2nqYTu3g46IlFTILuJEY5YpVgTpaW8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
