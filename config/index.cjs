453ba5270b17633488961976da907c939e73bc0d
yutuojin <116081538+yutuojin@users.noreply.github.com>
2022-11-24 11:01:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oZ-s-5l-ZC_TC-HmbtBfNEPowWno',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	yTwDUg1ujWqSuOkPZ4Ap8nuBToj3MGJEnwEJgZnpiaQ',
+      useTemplateId: 'NeWJcTqTtOW0Y2nqYTu3g46IlFTILuJEY5YpVgTpaW8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-22',
       festivals: [
