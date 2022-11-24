9328dfb8e7ee882b2b7b478cd1880db3c542edfe
yutuojin <116081538+yutuojin@users.noreply.github.com>
2022-11-24 11:13:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -34,19 +34,5 @@ const USER_CONFIG = {
       ],
     },
   ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'NeWJcTqTtOW0Y2nqYTu3g46IlFTILuJEY5YpVgTpaW8',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oZ-s-5l-ZC_TC-HmbtBfNEPowWno',
-    }
-  ],
-
-}
-
+  
 module.exports = USER_CONFIG
