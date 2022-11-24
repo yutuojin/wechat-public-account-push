84783ea6c2551c152686fb92e712d760b9e6b545
yutuojin <116081538+yutuojin@users.noreply.github.com>
2022-11-24 11:10:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx65ed5f4e79597cf3',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'fa9ddd01ce16c71c0dff639b7f63e513',
+  APP_SECRET: '',
 
   PROVINCE: '昆明',
   CITY: '嵩明',
@@ -21,19 +21,32 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'Yu',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oZ-s-5l-ZC_TC-HmbtBfNEPowWno',
+      id: 'wx65ed5f4e79597cf3',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'NeWJcTqTtOW0Y2nqYTu3g46IlFTILuJEY5YpVgTpaW8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '04-22',
+      horoscopeDate: '11-24',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2003', date: '04-22',
+          type: '生日', name: '喻拓进', year: '2003', date: '04-22',
         },
+      ],
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: 'NeWJcTqTtOW0Y2nqYTu3g46IlFTILuJEY5YpVgTpaW8',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oZ-s-5l-ZC_TC-HmbtBfNEPowWno',
+    }
   ],
 
 }
 
 module.exports = USER_CONFIG
-
